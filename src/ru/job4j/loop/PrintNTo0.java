26ce8a205af3d5362bfdbacc2995208728bc36cc
Алексей <ae560@mail.ru>
2020-11-05 06:31:31 +0300

    1.1.5. Циклы

---
+++ src/ru/job4j/loop/PrintNTo0.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class PrintNTo0 {
+    public static void out(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println((n - i));
+
+        }
+    }
+
+}
