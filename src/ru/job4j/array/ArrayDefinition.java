a0698869097a0292a4cc9ce66b1963cf03ad8cb9
Алексей <ae560@mail.ru>
2020-11-06 04:48:25 +0300

    6.0.1. Объявление массива. [#372543]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public void main() {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
